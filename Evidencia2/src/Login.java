7fb388a439004dc5649057078ef14e3631f89ab3
kom <karimcablem@hotmail.com>
2019-04-23 04:06:56 -0500

    creacion cliente

--- Evidencia2/src/Login.java
+++ Evidencia2/src/Login.java
@@ -4,6 +4,7 @@ import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
 import facebook4j.Friend;
 import facebook4j.Post;
+import facebook4j.PostUpdate;
 import facebook4j.ResponseList;
 import facebook4j.User;
 import facebook4j.auth.AccessToken;
@@ -14,12 +15,19 @@ public class Login {
 
 		facebook.setOAuthAppId("1066536996877003", "ec30ff2be302fefb66b0e8bfaab86e41");
 		facebook.setOAuthPermissions("email,publish_stream");
-		facebook.setOAuthAccessToken(new AccessToken("EAASqt8BZCXGkBAA0Y7xj1ApmWILmvQEZCqCoSv94bu3shyk3NA8C08N4Pok6h3Ew1ScGyEddoehVnxHUXndZAZAOQO7WTA25ZB3FoyhfwX5KwprMI8pRm0q7dWGHrnZA6drHP4wFIk1jeaPJ7ml0RQUn8xFDMZCNrZAJ7qJ1CZAKZAjRNAwCtHvMD5I6juv0davOtSA3vEPuJk0LEKiEWxxZCJBEXv0aJLk47ZASxkkHlxXUWQZDZD", null));
-		ResponseList<User> results = facebook.search();
+		facebook.setOAuthAccessToken(new AccessToken("EAAPKAn7WsssBAHZBZBZAo9z0PXiXU9ohVDtuQk47oNCrednwCHLTE4EUOgkLytISNxbmclPWepCEBnhXhzQejAnQzBuZAprbCaAbOzCB4X1Wi5mZALtWdhJU7Rc72ZAtLB9W5LB36ARTfZClxJ2g17JDODlexQv2URl9Y5O06LWgvbvDKoDi5kt9TLJNrMDKPJXbx9ROeAIZA2UmgAsL50ZApZBGSrGhwYqtBdxaDgMCJDSQZDZD", null));
 		
-		for(Object us:results) {
-			System.out.println((User)us);
+		System.out.println(facebook.callGetAPI("/me"));
+
+		System.out.println("\tGetting my posts\n");
+		ResponseList feed = facebook.getFeed();
+		for (Object post : feed) {
+		    System.out.println(post);
 		}
+		
+		PostUpdate pu= new PostUpdate("Hello World from Facebook4J.");
+		
+		facebook.postFeed(pu);
 
 	}
 }
